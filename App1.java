ae7c2c3bf38b2301d9244234c0fbfd7d582d387a
RajatWiley <hit19ece.rajatganguly@gmail.com>
2024-02-01 12:24:53 +0530

    Second commit

--- App1.java
+++ App1.java
@@ -1,5 +1,6 @@
 public class App1 {
     public static void main(String[] args) {
         System.out.println("Java App Pulled & Build by Jenkins");
+        System.out.println("Some more code for testing");
     }
 }
